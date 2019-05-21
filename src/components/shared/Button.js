eea326f608b68e818b76493e419a35fa08cd7d5f
Bogdan <bogdan7morozyuk@gmail.com>
2019-05-21 15:30:42 +0300

    Add "type" prop

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ active, children, onClick }) => (
-    <button 
-        className="btn btn-dark" 
-        disabled={active} 
-        onClick={onClick}
-    >
-        {children}
-    </button>
+const Button = ({ active, type, children, onClick }) => (
+  <button
+    className="btn btn-dark"
+    type={type}
+    disabled={active}
+    onClick={onClick}
+  >
+    {children}
+  </button>
 );
 
 Button.propTypes = {
-    active: PropTypes.bool,
-    children: PropTypes.any,
-    onClick: PropTypes.func
+  active: PropTypes.bool,
+  type: PropTypes.oneOf(['submit', 'reset', 'button']),
+  children: PropTypes.any.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+Button.defaultProps = {
+  active: false,
+  type: 'button'
 };
 
 export default Button;
